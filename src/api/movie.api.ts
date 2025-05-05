4022ead94ddd9052f9d269e5eb4090ae656e7442
Yurkes2020 <yurkes2020@gmail.com>
2025-05-05 23:15:12 +0300

    test api 1

--- src/api/movie.api.ts
+++ src/api/movie.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type {Movie} from "../types/movie.types.ts";
 
 
-const API_KEY = 'c14bfeaa94946e9ea8c19389f6842e46';
+const API_KEY = '';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 export const moviesAPI = {
